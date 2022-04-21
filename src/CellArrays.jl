9a76bf255d4a52dc496cb41ba3909d71d225f185
Samuel Omlin <samuel.omlin@cscs.ch>
2022-04-21 12:20:52 +0200

    create Module

--- src/CellArrays.jl
+++ src/CellArrays.jl
@@ -1,5 +1,13 @@
+"""
+Module CellArrays
+
+Provides support for an AbstractArray subtype `CellArray`, which represents arrays with cells that can contain logical arrays instead of single values; the data is stored in an optimal fashion for GPU HPC applications.
+
+# General overview and examples
+https://github.com/omlins/CellArray.jl
+"""
 module CellArrays
 
-# Write your package code here.
+include("CellArray.jl")
 
 end
