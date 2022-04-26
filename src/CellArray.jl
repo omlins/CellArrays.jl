df2a4701c5d597a312b4cff0fba514e0c9644c6a
Samuel Omlin <samuel.omlin@cscs.ch>
2022-04-26 11:23:08 +0200

    implement blocked storage

--- src/CellArray.jl
+++ src/CellArray.jl
@@ -5,57 +5,60 @@ Cell = Union{Number, SArray, FieldArray}
 struct CellArray{T<:Cell,N,T_array<:AbstractArray} <: AbstractArray{T,N}
     data::T_array
     dims::NTuple{N,Int}
+    blocklength::Int
 
-    function CellArray{T,N,T_array}(data::T_array, dims::NTuple{N,Int}) where {T<:Cell, N, T_array<:AbstractArray}
+    function CellArray{T,N,T_array}(data::T_array, dims::NTuple{N,Int}, blocklength::Int) where {T<:Cell, N, T_array<:AbstractArray}
         check_T(T)
         celldims = size(T)  # Note: size must be defined for type T (as it is e.g. for StaticArrays)
         T_elem = eltype(T)  # Note: eltype must be defined for type T (as it is e.g. for StaticArrays)
         if (eltype(data) != T_elem)                     @IncoherentArgumentError("eltype(data) must match eltype(T).") end
-        if (ndims(data) != 2)                           @ArgumentError("ndims(data) must be 2.") end
-        if (size(data) != (prod(dims), prod(celldims))) @IncoherentArgumentError("size(data) must match (prod(dims), prod(size(T))).") end
+        if (ndims(data) != 3)                           @ArgumentError("ndims(data) must be 3.") end
+        if (size(data) != (blocklength, prod(celldims)), ceil(Int,prod(dims)/blocklength)) @IncoherentArgumentError("size(data) must match (blocklength, prod(size(T)), ceil(prod(dims)/blocklength)).") end
         new{T,N,T_array}(data, dims)
     end
-    function CellArray{T,N}(data::T_array, dims::NTuple{N,Int}) where {T<:Cell, N, T_array<:AbstractArray}
+    function CellArray{T,N}(data::T_array, dims::NTuple{N,Int}, blocklength::Int) where {T<:Cell, N, T_array<:AbstractArray}
         CellArray{T,N,T_array}(data, dims)
     end
-    function CellArray{T,N}(dims::NTuple{N,Int}, T_array::Type{<:AbstractArray}) where {T<:Cell, N}
+    function CellArray{T,N}(dims::NTuple{N,Int}, blocklength::Int, T_array::Type{<:AbstractArray}) where {T<:Cell, N}
         check_T(T)
         celldims = size(T)  # Note: size must be defined for type T (as it is e.g. for StaticArrays)
         T_elem = eltype(T)  # Note: eltype must be defined for type T (as it is e.g. for StaticArrays)
-        data = T_array{T_elem,2}(undef, prod(dims), prod(celldims))
-        CellArray{T,N,T_array}(data, dims)
+        data = T_array{T_elem,3}(undef, blocklength, prod(celldims)), ceil(Int,prod(dims)/blocklength))
+        CellArray{T,N,T_array}(data, dims, blocklength)
     end
-    function CellArray{T}(dims::NTuple{N,Int}, T_array::Type{<:AbstractArray}) where {T<:Cell, N}
-        CellArray{T,N}(dims, T_array)
+    function CellArray{T}(dims::NTuple{N,Int}, blocklength::Int, T_array::Type{<:AbstractArray}) where {T<:Cell, N}
+        CellArray{T,N}(dims, blocklength, T_array)
     end
 end
 
 CuCellArray{T,N}  = CellArray{T,N,CuArray}
 ROCCellArray{T,N} = CellArray{T,N,ROCArray}
 
-CellArray(::Type{T}, dims::NTuple{N,Int}) where {T<:Cell,N}                  = (check_T(T); CellArray{T,N}(dims, Array))
-CellArray(::Type{T}, dims::Int...) where {T<:Cell}                           = (check_T(T); CellArray(T, dims))
-CuCellArray(::Type{T}, dims::NTuple{N,Int}) where {T<:Cell,N}                = (check_T(T); CellArray{T,N}(dims, CuArray))
-CuCellArray(::Type{T}, dims::Int...) where {T<:Cell}                         = (check_T(T); CuCellArray(T, dims))
-ROCCellArray(::Type{T}, dims::NTuple{N,Int}) where {T<:Cell,N}               = (check_T(T); CellArray{T,N}(dims, ROCArray))
-ROCCellArray(::Type{T}, dims::Int...) where {T<:Cell}                        = (check_T(T); ROCCellArray(T, dims))
+CellArray(::Type{T}, dims::NTuple{N,Int}, blocklength=prod(dims)) where {T<:Cell,N}    = (check_T(T); CellArray{T,N}(dims, blocklength, Array))
+CellArray(::Type{T}, dims::Int...) where {T<:Cell}                                     = (check_T(T); CellArray(T, dims))
+CuCellArray(::Type{T}, dims::NTuple{N,Int}, blocklength=prod(dims)) where {T<:Cell,N}  = (check_T(T); CellArray{T,N}(dims, blocklength, CuArray))
+CuCellArray(::Type{T}, dims::Int...) where {T<:Cell}                                   = (check_T(T); CuCellArray(T, dims))
+ROCCellArray(::Type{T}, dims::NTuple{N,Int}, blocklength=prod(dims)) where {T<:Cell,N} = (check_T(T); CellArray{T,N}(dims, blocklength, ROCArray))
+ROCCellArray(::Type{T}, dims::Int...) where {T<:Cell}                                  = (check_T(T); ROCCellArray(T, dims))
+
+@inline function Base.similar(A::CellArray{T0,N0,T_array}, ::Type{T}, dims::NTuple{N,Int}) where {T0,N0,T_array,T<:Cell,N}
+    blocklength = (dims==A.dims) ? A.blocklength : prod(dims)  # Note: if the `dims` of the new array are to be the same as those of the old array, then the same blocklength as in the old array are used; else, the default blocklength is used.
+    CellArray{T,N}(dims, blocklength, T_array)
+end
 
 @inline Base.IndexStyle(::Type{<:CellArray})                                 = IndexLinear()
 @inline Base.size(T::Type{<:Number}, args...)                                = 1
 @inline Base.size(A::CellArray)                                              = A.dims
-@inline Base.getindex(A::CellArray{T}, i::Int) where {T <: Number}           = T(A.data[i,1])
-@inline Base.getindex(A::CellArray{T}, i::Int) where {T}                     = T(getindex(A.data, i, j) for j=1:length(T)) # NOTE:The same fails on GPU if convert is used.
-@inline Base.setindex!(A::CellArray{T}, x::Number, i::Int) where {T<:Number} = (A.data[i] = x; return)
-@inline Base.setindex!(A::CellArray{T}, X::T, i::Int) where {T}              = (for j=1:length(T) A.data[i,j] = X[j] end; return)
-
-@inline function Base.similar(A::CellArray{T0,N0,T_array}, ::Type{T}, dims::NTuple{N,Int}) where {T0,N0,T_array,T<:Cell,N} 
-    CellArray{T,N}(dims, T_array)
-end
+@inline Base.getindex(A::CellArray{T}, i::Int) where {T <: Number}           = T(A.data[i%A.blocklength, 1, i÷A.blocklength])
+@inline Base.getindex(A::CellArray{T}, i::Int) where {T}                     = T(getindex(A.data, i%A.blocklength, j, i÷A.blocklength) for j=1:length(T)) # NOTE:The same fails on GPU if convert is used.
+@inline Base.setindex!(A::CellArray{T}, x::Number, i::Int) where {T<:Number} = (A.data[i%A.blocklength, 1, i÷A.blocklength] = x; return)
+@inline Base.setindex!(A::CellArray{T}, X::T, i::Int) where {T}              = (for j=1:length(T) A.data[i%A.blocklength, j, i÷A.blocklength] = X[j] end; return)
 
 @inline cellsize(A::AbstractArray)                                           = size(eltype(A))
 @inline cellsize(A::AbstractArray, i::Int)                                   = cellsize(A)[i]
+@inline blocklength(A::CellArray)                                            = A.blocklength
 
-Adapt.adapt_structure(to, A::CellArray{T,N}) where {T,N}                     = CellArray{T,N}(adapt(to, A.data), A.dims)
+Adapt.adapt_structure(to, A::CellArray{T,N}) where {T,N}                     = CellArray{T,N}(adapt(to, A.data), A.dims, A.blocklength)
 
 function check_T(::Type{T}) where {T}
     if !isbitstype(T) @ArgumentError("the celltype, `T`, must be a bitstype.") end # Note: This test is required as FieldArray can be mutable and thus not bitstype (and ismutable() is for values not types...). The following tests would currently not be required as the current definition of the Cell type implies the tests to succeed.
