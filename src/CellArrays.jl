d4a57d7f018386a22068a8833ce73ee00b2cb22f
Samuel Omlin <samuel.omlin@cscs.ch>
2022-05-09 15:46:15 +0200

    improve module doc

--- src/CellArrays.jl
+++ src/CellArrays.jl
@@ -16,7 +16,7 @@ https://github.com/omlins/CellArray.jl
 - [`cellsize`](@ref)
 - [`blocklength`](@ref)
 
-To see a description of a constructor or function type `?<functionname>`.
+To see a description of a constructor or function type `?<constructorname>` or `?<functionname>`, respectively.
 
 !!! note "Performance note"
     If a CellArray's cells contain more than what fits into registers, the performance on Nvidia GPUs will deviate from the optimal, if access is not performed by accessing the cells' values individually to reduce register pressure.
