0e5a634f896f3f79bf20e84bd764371dad5e11ad
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-15 12:19:25 +0200

    improve make formatting

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,11 @@ using Documenter
 DocMeta.setdocmeta!(CellArrays, :DocTestSetup, :(using CellArrays); recursive=true)
 
 makedocs(;
-    modules=[CellArrays],
-    authors="Samuel Omlin",
-    repo="https://github.com/omlins/CellArrays.jl/blob/{commit}{path}#{line}",
-    sitename="CellArrays.jl",
-    format=Documenter.HTML(;
+    modules  = [CellArrays],
+    authors  = "Samuel Omlin",
+    repo     = "https://github.com/omlins/CellArrays.jl/blob/{commit}{path}#{line}",
+    sitename = "CellArrays.jl",
+    format   = Documenter.HTML(;
         prettyurls       = get(ENV, "CI", "false") == "true",
         canonical        = "https://omlins.github.io/CellArrays.jl",
         collapselevel    = 1,
@@ -16,7 +16,7 @@ makedocs(;
         #assets           = [asset("https://img.shields.io/github/stars/omlins/CellArrays.jl.svg", class = :ico)],
         #warn_outdated    = true,
     ),
-    pages=[
+    pages   = [
         "Introduction"  => "index.md",
         "Usage"         => "usage/usage.md",
         "Examples"      => "examples/examples.md",
