5f7b8dc38b153bca1331084ece5a3d68963f17c7
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-19 18:03:06 +0200

    add reflinks as .jl file

---
+++ docs/reflinks.jl
@@ -0,0 +1,8 @@
+reflinks = Dict(
+    "[Adapt.jl]"           => "https://github.com/JuliaGPU/Adapt.jl",
+    "[AMDGPU.jl]"          => "https://github.com/JuliaGPU/AMDGPU.jl",
+    "[CellArrays.jl]"      => "https://github.com/omlins/CellArrays.jl",
+    "[CUDA.jl]"            => "https://github.com/JuliaGPU/CUDA.jl",
+    "[ParallelStencil.jl]" => "https://github.com/omlins/ParallelStencil.jl",
+    "[StaticArrays.jl]"    => "https://github.com/JuliaArrays/StaticArrays.jl",
+)
