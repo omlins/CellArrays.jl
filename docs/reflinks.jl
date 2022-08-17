40dabf0a530bfe445a9fc3f181b49f04971c30fd
Samuel Omlin <samuel.omlin@cscs.ch>
2022-08-17 19:31:41 +0200

    add reflinks

--- docs/reflinks.jl
+++ docs/reflinks.jl
@@ -1,9 +1,10 @@
 reflinks = Dict(
-    "[Adapt.jl]"           => "https://github.com/JuliaGPU/Adapt.jl",
-    "[AMDGPU.jl]"          => "https://github.com/JuliaGPU/AMDGPU.jl",
-    "[CellArrays.jl]"      => "https://github.com/omlins/CellArrays.jl",
-    "[CUDA.jl]"            => "https://github.com/JuliaGPU/CUDA.jl",
-    "[Julia REPL]"         => "https://docs.julialang.org/en/v1/stdlib/REPL/",
-    "[ParallelStencil.jl]" => "https://github.com/omlins/ParallelStencil.jl",
-    "[StaticArrays.jl]"    => "https://github.com/JuliaArrays/StaticArrays.jl",
+    "[Adapt.jl]"              => "https://github.com/JuliaGPU/Adapt.jl",
+    "[AMDGPU.jl]"             => "https://github.com/JuliaGPU/AMDGPU.jl",
+    "[CellArrays.jl]"         => "https://github.com/omlins/CellArrays.jl",
+    "[CUDA.jl]"               => "https://github.com/JuliaGPU/CUDA.jl",
+    "[Julia package manager]" => "https://docs.julialang.org/en/v1/stdlib/Pkg/",
+    "[Julia REPL]"            => "https://docs.julialang.org/en/v1/stdlib/REPL/",
+    "[ParallelStencil.jl]"    => "https://github.com/omlins/ParallelStencil.jl",
+    "[StaticArrays.jl]"       => "https://github.com/JuliaArrays/StaticArrays.jl",
 )
