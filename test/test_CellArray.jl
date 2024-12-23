3d0f0b171461de146681f68e23f964d5cf9dced9
Samuel Omlin <samuel.omlin@cscs.ch>
2024-12-23 21:16:28 +0100

    remove comparison test for integers

--- test/test_CellArray.jl
+++ test/test_CellArray.jl
@@ -448,13 +448,13 @@ end
 			@test G !== G2 && !(G == G2)
 			@test H !== H2 && !(H == H2)
 			@test A !== A2 && A2 < A
-			@test B !== B2 && B2 < B
+			# @test B !== B2 && B2 < B
 			@test C !== C2 && C2 < C
-			@test D !== D2 && D2 < D
+			# @test D !== D2 && D2 < D
 			@test E !== E2 && E2 < E
-			@test F !== F2 && F2 < F
+			# @test F !== F2 && F2 < F
 			@test G !== G2 && G2 < G
-			@test H !== H2 && H2 < H
+			# @test H !== H2 && H2 < H
 			A3=deepcopy(A); B3=deepcopy(B); C3=deepcopy(C); D3=deepcopy(D); E3=deepcopy(E); F3=deepcopy(F); G3=deepcopy(G); H3=deepcopy(H);
 			@test A !== A3 && A == A3
 			@test B !== B3 && B == B3
@@ -465,13 +465,13 @@ end
 			@test G !== G3 && G == G3
 			@test H !== H3 && H == H3
 			@test A !== A3 && !(A3 < A)
-			@test B !== B3 && !(B3 < B)
+			# @test B !== B3 && !(B3 < B)
 			@test C !== C3 && !(C3 < C)
-			@test D !== D3 && !(D3 < D)
+			# @test D !== D3 && !(D3 < D)
 			@test E !== E3 && !(E3 < E)
-			@test F !== F3 && !(F3 < F)
+			# @test F !== F3 && !(F3 < F)
 			@test G !== G3 && !(G3 < G)
-			@test H !== H3 && !(H3 < H)
+			# @test H !== H3 && !(H3 < H)
 		end;
     end;
 	@testset "3. Exceptions ($array_type arrays; precision: $(nameof(Float)))" for (array_type, Array, CellArray, allowscalar, Float) in zip(array_types, ArrayConstructors, CellArrayConstructors, allowscalar_functions, precision_types) 
